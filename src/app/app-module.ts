ba1384631803cfa97903f25a64026efb5f6d8b03
Rogerio <barrucalleortiz@gmail.com>
2026-04-16 10:49:16 -0300

    Criando cabelaho e ajustando config do angular

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Cabecalho } from './componentes/cabecalho/cabecalho';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Cabecalho],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
