fc52ceb1786e075a6b1e24e017873229279dfd3e
Rogerio <barrucalleortiz@gmail.com>
2026-04-16 11:29:11 -0300

    Criação do rodapé do projeto

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
+import { Rodape } from './componentes/rodape/rodape';
 
 @NgModule({
-  declarations: [App, Cabecalho],
+  declarations: [App, Cabecalho, Rodape],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
---
+++ src/app/componentes/rodape/rodape.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rodape',
+  standalone: false,
+  templateUrl: './rodape.html',
+  styleUrl: './rodape.css',
+})
+export class Rodape {}
