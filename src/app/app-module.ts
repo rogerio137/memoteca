e2e5f80bd41d5cdfe213fe348fb8f0460033a717
Rogerio <barrucalleortiz@gmail.com>
2026-04-16 12:02:45 -0300

    Criação dos elementos da tela

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
 import { Rodape } from './componentes/rodape/rodape';
+import { CriarPensamento } from './componentes/pensamentos/criar-pensamento/criar-pensamento';
 
 @NgModule({
-  declarations: [App, Cabecalho, Rodape],
+  declarations: [App, Cabecalho, Rodape, CriarPensamento],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
